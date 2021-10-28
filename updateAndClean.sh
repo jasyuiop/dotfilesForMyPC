a15ee91b08ee26b24e5840163f8fbb5d77c783c3
M.Emre Nefesli <emrenefesli@outlook.com>
2021-10-28 18:23:55 +0300

    ...

---
+++ updateAndClean.sh
@@ -0,0 +1,22 @@
+#!/bin/env zsh
+set -uo pipefail
+
+printf "Running pacman update...\n"
+sudo pacman -Syu;
+
+printf "Removing orphans...\\n"
+sudo pacman -Rns $(pacman -Qtdq)
+
+sudo pacdiff
+
+printf "Cleaning up pacman cache...\\n"
+sudo pacman -Sc
+printf "Done\\n\\n"
+
+printf "Cleaning journal..\n"
+printf "Before journal\n"
+journalctl --disk-usage
+printf "Cleaning\n”"
+journalctl --vacuum-time=7d
+printf "After journal\n"
+journalctl --disk-usage
